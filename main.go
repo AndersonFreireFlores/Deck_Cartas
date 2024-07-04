5e87d9d2fced25d7995b20ad0a6d6266e7da8e38
Anderson Freire Flores <andersonfflores75@gmail.com>
2024-07-04 13:46:54 -0300

    newDeckFromFile function

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := newDeck()
-	cards.saveToFile("minhas_cartas")
 
-	hand, remainigCards := deal(cards, 5)
-
-	fmt.Println("Hand:", hand)
-	fmt.Println("RemainigCards:", remainigCards)
-
+	cards := newDeckFromFile("minhas_cartas")
+	cards.print()
 }
